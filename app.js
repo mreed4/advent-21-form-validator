03dcbda5707918d36fce513ca15dfdf38ef4e957
Matthew <78713317+mreed4@users.noreply.github.com>
2024-01-15 13:32:37 -0800

    Additional logic, improved CSS

--- app.js
+++ app.js
@@ -1,20 +1,65 @@
 const allFields = document.querySelectorAll(".field");
+const allShowPasswordButtons = document.querySelectorAll(".show-hide");
 const form = document.querySelector("form");
+const submitButton = document.querySelector("button[type='submit']");
 
 (function init() {
   hideValidationDivs();
-  disableFormSubmit();
+  initEventListeners();
 })();
 
-function disableFormSubmit() {
+function initEventListeners() {
   form.addEventListener("submit", (e) => {
     e.preventDefault();
+    console.log("Form submitted");
+  });
+
+  allShowPasswordButtons.forEach((button) => {
+    button.addEventListener("click", showHidePassword);
+  });
+
+  document.addEventListener("keyup", (event) => {
+    if (event.altKey && event.key === "s") {
+      console.log("alt + s");
+      showHidePassword(event);
+    }
   });
 }
 
+function toggleSubmitButton() {
+  /* */
+  const isDisabled = submitButton.hasAttribute("disabled");
+
+  if (isDisabled) {
+    submitButton.removeAttribute("disabled");
+  }
+
+  if (!isDisabled) {
+    submitButton.setAttribute("disabled", "disabled");
+  }
+}
+
+function showHidePassword(event) {
+  /* */
+  const parentElement = event.target.parentElement;
+  const isVisible = parentElement.classList.contains("show");
+  const input = parentElement.querySelector("input");
+
+  if (isVisible) {
+    parentElement.classList.remove("show");
+    input.type = "password";
+  }
+
+  if (!isVisible) {
+    parentElement.classList.add("show");
+    input.type = "text";
+  }
+}
+
 function hideValidationDivs() {
+  /* */
   [...allFields]
-    .filter((field) => field.childElementCount > 1)
+    .filter((field) => field.childElementCount > 1) // filter out fields with only one child
     .forEach((field) => {
       const validationDivs = [...field.children].filter((child) => child.localName === "div");
       const [errorDiv, successDiv] = validationDivs;
@@ -24,52 +69,84 @@ function hideValidationDivs() {
       });
 
       field.addEventListener("input", (event) => {
-        handleInputBlur(event, errorDiv, successDiv, event.target);
+        validateField(event, errorDiv, successDiv);
       });
     });
 }
 
-function handleInputBlur(event, errorDiv, successDiv, input) {
+function validateField(event, errorDiv, successDiv) {
   /* */
-  if (event.target.id === "email") {
-    const email = input.value;
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+  const { id, value } = event.target;
 
-    if (emailRegex.test(email)) {
-      successDiv.classList.remove("display-none");
-      errorDiv.classList.add("display-none");
-    }
-
-    if (!emailRegex.test(email)) {
-      successDiv.classList.add("display-none");
-      errorDiv.classList.remove("display-none");
-    }
+  if (id === "email") {
+    emailTest(value, errorDiv, successDiv);
   }
 
-  if (event.target.id !== "email") {
-    if (event.target.value.length >= 2) {
-      successDiv.classList.remove("display-none");
-      errorDiv.classList.add("display-none");
-    }
-
-    if (event.target.value.length < 2) {
-      successDiv.classList.add("display-none");
-      errorDiv.classList.remove("display-none");
-    }
+  if (id !== "email") {
+    defaultTest(value.length, errorDiv, successDiv);
   }
 
-  if (event.target.id === "confirm-password") {
-    const password = document.querySelector("#password").value;
-    const confirmPassword = document.querySelector("#confirm-password").value;
+  if (id === "password") {
+    passwordTest(value, errorDiv, successDiv);
+  }
 
-    if (password === confirmPassword) {
-      successDiv.classList.remove("display-none");
-      errorDiv.classList.add("display-none");
-    }
+  if (id === "confirm-password") {
+    testPasswordsMatch(errorDiv, successDiv);
+  }
+}
 
-    if (password !== confirmPassword) {
-      successDiv.classList.add("display-none");
-      errorDiv.classList.remove("display-none");
-    }
+function emailTest(email, errorDiv, successDiv) {
+  /* */
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+
+  if (emailRegex.test(email)) {
+    successDiv.classList.remove("display-none");
+    errorDiv.classList.add("display-none");
+  }
+
+  if (!emailRegex.test(email)) {
+    successDiv.classList.add("display-none");
+    errorDiv.classList.remove("display-none");
+  }
+}
+
+function defaultTest(length, errorDiv, successDiv) {
+  /* */
+  if (length >= 2) {
+    successDiv.classList.remove("display-none");
+    errorDiv.classList.add("display-none");
+  }
+
+  if (length < 2) {
+    successDiv.classList.add("display-none");
+    errorDiv.classList.remove("display-none");
+  }
+}
+
+function passwordTest(password, errorDiv, successDiv) {
+  /* */
+  if (password.length >= 8) {
+    successDiv.classList.remove("display-none");
+    errorDiv.classList.add("display-none");
+  }
+
+  if (password.length < 8) {
+    successDiv.classList.add("display-none");
+    errorDiv.classList.remove("display-none");
+  }
+}
+function testPasswordsMatch(errorDiv, successDiv) {
+  /* */
+  const password = document.querySelector("#password").value;
+  const confirmPassword = document.querySelector("#confirm-password").value;
+
+  if (password === confirmPassword) {
+    successDiv.classList.remove("display-none");
+    errorDiv.classList.add("display-none");
+  }
+
+  if (password !== confirmPassword) {
+    successDiv.classList.add("display-none");
+    errorDiv.classList.remove("display-none");
   }
 }
